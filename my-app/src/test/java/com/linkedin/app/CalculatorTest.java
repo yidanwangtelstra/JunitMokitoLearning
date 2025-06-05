b2b22e436250fb5a5b3c3a339b996f4a51ee109e
Yidan Wang <yidan.wang@team.telstra.com>
2025-06-05 17:07:15 +0930

    testing exceptions

--- my-app/src/test/java/com/linkedin/app/CalculatorTest.java
+++ my-app/src/test/java/com/linkedin/app/CalculatorTest.java
@@ -1,13 +1,8 @@
 package com.linkedin.app;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
@@ -40,12 +35,10 @@ public class CalculatorTest {
   @Test
   public void divideByZero() {
     Integer result = null;
-    try {
-      result = underTest.divide(6, 0);
-    } catch (IllegalArgumentException e) {
-      // Division by zero occurred
-    }
-    assertNull(result, "Division by zero should result in null");
+    IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> {
+      underTest.divide(6, 0);
+    });
+    assertEquals("Cannot divide by zero!", e.getMessage());
   }
 
   @Test
