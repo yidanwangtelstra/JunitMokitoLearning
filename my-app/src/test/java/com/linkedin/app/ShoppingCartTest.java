822fccf49f5462fe898b6c4ae2fc7937e34cbb61
Yidan Wang <yidan.wang@team.telstra.com>
2025-06-05 16:56:49 +0930

    added setup and teardown

--- my-app/src/test/java/com/linkedin/app/ShoppingCartTest.java
+++ my-app/src/test/java/com/linkedin/app/ShoppingCartTest.java
@@ -2,10 +2,18 @@ package com.linkedin.app;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ShoppingCartTest {
-  private static ShoppingCart cart = new ShoppingCart();
+  private static ShoppingCart cart;
+
+  @BeforeEach
+  public void setUp(){
+    cart = new ShoppingCart();
+  }
 
   @Test
   public void addItem() {
@@ -19,5 +27,10 @@ public class ShoppingCartTest {
     cart.addItem("Product A", 2);
     cart.removeItem("Product A", 1);
     assertEquals(1, cart.getTotalItems());
+  }
+
+  @AfterEach
+  public void tearDown(){
+    cart = null;
   }
 }
