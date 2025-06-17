4d16ad9985616c879e4477a2539fd21ba6989785
Yidan Wang <yidan.wang@team.telstra.com>
2025-06-17 12:08:48 +0930

    Add Task and eneter green phase

--- my-app/src/main/java/com/linkedin/app/TaskManager.java
+++ my-app/src/main/java/com/linkedin/app/TaskManager.java
@@ -1,13 +1,19 @@
 package com.linkedin.app;
 
+import java.util.HashMap;
+
 public class TaskManager {
+  HashMap<Integer, Task> tasks;
+  public TaskManager(){
+    this.tasks = new HashMap<>();
+  }
 
   public void add(Task task) {
-
+    tasks.put(task.getId(), task);
   }
 
   public boolean exists(int id) {
-    return false;
+    return tasks.containsKey(id);
   }
 
 }
