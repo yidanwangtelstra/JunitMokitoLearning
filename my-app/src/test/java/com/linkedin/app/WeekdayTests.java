f43654bf2980629158208a9abe1353159683aa16
Yidan Wang <yidan.wang@team.telstra.com>
2025-06-18 15:22:36 +0930

    Conditional test execution based on custom conditions

--- my-app/src/test/java/com/linkedin/app/WeekdayTests.java
+++ my-app/src/test/java/com/linkedin/app/WeekdayTests.java
@@ -4,16 +4,19 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIf;
+import org.junit.jupiter.api.condition.EnabledIf;
 
 public class WeekdayTests {
 
     @Test
+    @EnabledIf("isWeekday")
     void weekdays() {
         System.out.println("This test runs only on weekdays.");
-        // Your test logic here
     }
 
     @Test
+    @DisabledIf("isWeekday")
     void weekends() {
         System.out.println("This test runs only on weekends.");
         // Your test logic here
