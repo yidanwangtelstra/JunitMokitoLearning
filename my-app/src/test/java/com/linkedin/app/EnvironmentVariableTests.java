b9286d8d4bc5dd2810d80c0408fa8fb2faad0bfc
Yidan Wang <yidan.wang@team.telstra.com>
2025-06-18 15:05:38 +0930

    Conditional test execution based on environment variables

--- my-app/src/test/java/com/linkedin/app/EnvironmentVariableTests.java
+++ my-app/src/test/java/com/linkedin/app/EnvironmentVariableTests.java
@@ -1,18 +1,20 @@
 package com.linkedin.app;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
 public class EnvironmentVariableTests {
 
   @Test
+  @EnabledIfEnvironmentVariable(named = "ENVIRONMENT", matches = "development")
   void developmentEnvironment() {
     System.out.println("This test runs only in the development environment.");
-    // Your test logic here
   }
 
   @Test
+  @DisabledIfEnvironmentVariable(named = "ENVIRONMENT", matches = "development")
   void productionEnvironment() {
     System.out.println("This test does not run in the production environment.");
-    // Your test logic here
   }
 }
