1aeb530c85fc6c48a80b00e050b362d0681dd5ed
Yidan Wang <yidan.wang@team.telstra.com>
2025-06-21 16:26:05 +0930

    @Timeout

--- my-app/src/test/java/com/linkedin/app/TimeoutTest.java
+++ my-app/src/test/java/com/linkedin/app/TimeoutTest.java
@@ -1,12 +1,14 @@
 package com.linkedin.app;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 public class TimeoutTest {
 
   @Test
+  @Timeout(value =3)
   void methodWithTimeout() throws InterruptedException {
     // Perform test logic that should complete within 5 seconds
-    Thread.sleep(6000); // Simulating a lengthy operation
+    Thread.sleep(2000); // Simulating a lengthy operation
   }
 }
