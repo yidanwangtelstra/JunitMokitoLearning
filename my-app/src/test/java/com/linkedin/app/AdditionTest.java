8d721929e86350b923fdb05f8495656e48a6f8f5
Yidan Wang <yidan.wang@team.telstra.com>
2025-06-21 16:00:53 +0930

    Add a display name to your test using @DisplayName()

--- my-app/src/test/java/com/linkedin/app/AdditionTest.java
+++ my-app/src/test/java/com/linkedin/app/AdditionTest.java
@@ -2,11 +2,13 @@ package com.linkedin.app;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class AdditionTest {
 
   @Test
+  @DisplayName("Test should return correct addtion result 4")
   void addition() {
     int result = 2 + 2;
     assertEquals(4, result, "2 + 2 should equal 4");
