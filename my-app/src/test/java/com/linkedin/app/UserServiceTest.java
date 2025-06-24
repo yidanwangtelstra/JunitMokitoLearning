ff20350aa25239c2997aee4a02dd2f83696cbf15
Yidan Wang <yidan.wang@team.telstra.com>
2025-06-24 15:48:10 +0930

    mock() = @Mock

--- my-app/src/test/java/com/linkedin/app/UserServiceTest.java
+++ my-app/src/test/java/com/linkedin/app/UserServiceTest.java
@@ -6,17 +6,27 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
+  @Mock
+  UserRepository userRepositoryMock;
+
+  @InjectMocks
+  UserService underTest;
 
   @Test
   public void findUserById() {
-    UserRepository userRepositoryMock = mock(UserRepository.class);
+//    UserRepository userRepositoryMock = mock(UserRepository.class);
 
     User mockUser = new User("1", "John Doe");
     when(userRepositoryMock.findById("1")).thenReturn(mockUser);
 
-    UserService underTest = new UserService(userRepositoryMock);
+//    UserService underTest = new UserService(userRepositoryMock);
 
     User result = underTest.findUserById("1");
 
