4158fd6337e04b99362d182870b9c499ebbe04b7
Yidan Wang <yidan.wang@team.telstra.com>
2025-06-25 15:49:30 +0930

    Challenge tackled

---
+++ my-app/src/test/java/com/linkedin/app/OrderServiceTest.java
@@ -0,0 +1,74 @@
+package com.linkedin.app;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class OrderServiceTest {
+
+    private static final String CUSTOMER_NAME = "Yidan Wang";
+    @InjectMocks
+    OrderService orderService;
+
+    @Mock
+    OrderRepository orderRepository;
+
+    @Test
+    public void createOrderTest() {
+        Order result = orderService.createOrder(CUSTOMER_NAME);
+        verify(orderRepository).save(any(Order.class));
+        assertEquals(CUSTOMER_NAME, result.getCustomerName());
+
+    }
+
+    @Test
+    public void updateOrderTestSuccess() {
+        Order order = orderService.createOrder(CUSTOMER_NAME);
+        when(orderRepository.findById(anyInt())).thenReturn(order);
+
+        orderService.updateOrder(order);
+
+        verify(orderRepository).findById(anyInt());
+        verify(orderRepository, times(2)).save(any(Order.class));
+    }
+
+    @Test
+    public void updateOrderTestFailure(){
+        Order order = orderService.createOrder(CUSTOMER_NAME);
+        when(orderRepository.findById(anyInt())).thenReturn(null);
+
+        IllegalArgumentException e = assertThrows( IllegalArgumentException.class, () -> {
+            orderService.updateOrder(order);
+        });
+        assertEquals("Order not found", e.getMessage());
+    }
+
+    @Test
+    public void getOrderTestFailure(){
+        when(orderRepository.findById(anyInt())).thenReturn(null);
+
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> {
+            orderService.getOrder(1);
+        });
+        assertEquals("Order not found", e.getMessage());
+    }
+
+    @Test
+    public void getOrderTestSuccess() {
+        Order order = orderService.createOrder(CUSTOMER_NAME);
+        when(orderRepository.findById(anyInt())).thenReturn(order);
+        verify(orderRepository).save(any(Order.class));
+        assertEquals(order, orderService.getOrder(1));
+    }
+}
