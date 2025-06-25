298bcaa09239ee7aebe2a3839b1e2024a0662092
Yidan Wang <yidan.wang@team.telstra.com>
2025-06-25 14:54:42 +0930

    Mock a static method with Mockito

--- my-app/src/test/java/com/linkedin/app/UtilityTest.java
+++ my-app/src/test/java/com/linkedin/app/UtilityTest.java
@@ -1,11 +1,19 @@
 package com.linkedin.app;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UtilityTest {
 
     @Test
     public void celsiusToFahrenheit() {
-
+        try(MockedStatic<Utility> utility  = Mockito.mockStatic(Utility.class)){
+            utility.when(() -> Utility.celsiusToFahrenheit(0))
+                    .thenReturn(32.0);
+            assertEquals(32.0, Utility.celsiusToFahrenheit(0));
+        }
     }
 }
