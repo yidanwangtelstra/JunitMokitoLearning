9ade72549bf3712c97d9594efb9d619ba9a930de
Yidan Wang <yidan.wang@team.telstra.com>
2025-06-25 12:53:34 +0930

    Test void methods with Mockito using doThrow(...).when(), instead of when().thenThrows()

--- my-app/src/test/java/com/linkedin/app/UserNotificationServiceTest.java
+++ my-app/src/test/java/com/linkedin/app/UserNotificationServiceTest.java
@@ -1,5 +1,6 @@
 package com.linkedin.app;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.junit.jupiter.api.Test;
@@ -7,6 +8,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class UserNotificationServiceTest {
@@ -25,11 +31,21 @@ public class UserNotificationServiceTest {
 
     @Test
     void notifyUser() throws EmailException {
-
+        underTest.notifyUser(EMAIL, MESSAGE);
+        verify(emailService).sendEmail(anyString(), anyString(), anyString());
+        verify(logger).info("Attempting to send notification to " + EMAIL);
+        verify(logger).info("Notification sent to " + EMAIL);
     }
 
     @Test
     void notifyUser_EmailException() throws EmailException {
+        doThrow(new EmailException("Email sending failed"))
+                .when(emailService).sendEmail(anyString(), anyString(), anyString());
 
+//        when(emailService.sendEmail(anyString(), anyString(), anyString()).thenThrow(new EmailException("Email sending failed")));
+
+        underTest.notifyUser(EMAIL, MESSAGE);
+        verify(emailService).sendEmail(anyString(), anyString(), anyString());
+        verify(logger).log(eq(Level.SEVERE), eq("Failed to send email to " + EMAIL), any(EmailException.class));
     }
 }
